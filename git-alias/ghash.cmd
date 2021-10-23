74a4b563a8951f987216dd6df43fe6f228b82571
Lussac Zheng <lussaczheng@gmail.com>
2021-10-23 21:10:25 +0800

    feat: add useful aliases for git

---
+++ git-alias/ghash.cmd
@@ -0,0 +1,5 @@
+@REM git --no-pager log -n1 --format=%%h
+@REM git --no-pager log -n1 --format=%%H
+
+@git rev-parse --short HEAD
+@git rev-parse HEAD
---
+++ git-alias/glos.cmd
@@ -0,0 +1,3 @@
+@REM git log --graph --pretty="%Cred%h%Creset  %Cgreen%ci%Creset %C(auto)%d%Creset%n %s" --stat
+@REM git log --graph --pretty="%Cblue%h%Creset  %Cgreen%ci%Creset %C(auto)%d%Creset%n %Cred%s%Creset" --stat
+@git log --graph --pretty="%%Cblue%%h%%Creset  %%Cgreen%%ci%%Creset %%C(auto)%%d%%Creset%%n %%Cred%%s%%Creset" --stat

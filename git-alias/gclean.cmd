5697a95eceb115a02359f750c54fa02405ba06d2
Lussac Zheng <lussaczheng@gmail.com>
2021-10-30 21:36:41 +0800

    feat: add git-alias `gclean` - remove dangling commits

---
+++ git-alias/gclean.cmd
@@ -0,0 +1,3 @@
+@REM https://stackoverflow.com/questions/3765234/listing-and-deleting-git-commits-that-are-under-no-branch-dangling
+@git reflog expire --expire-unreachable=now --all
+@git gc --prune=now
